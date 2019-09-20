00b93a9f59dd4b20b20cd1a33e2f868a510b1dcb
Udai3858 <udaiveer056@gmail.com>
2019-09-20 13:36:37 -0400

    findsethub

--- bash/findsetidfiles.sh
+++ bash/findsetidfiles.sh
@@ -12,4 +12,8 @@
 echo "Setuid files:"
 echo "============="
 find / -type f -executable -perm -4000 -ls 2>/dev/null | sort -k 3
-echo ""
+echo "-setgid files"
+find / -type f -executable -perm -2000 -ls 2>/dev/null | sort -k 3
+
+echo"----"
+find /2>dev/null -type f -exec ls -l --block-size=M {} + | sort -hr -k 5 | head -n 10 | awk '{print $5 ,$3, $9}'

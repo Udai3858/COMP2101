e3f1061fffbdb3ce3ca2ac59eeb179af7e79e3cd
Udai3858 <udaiveer056@gmail.com>
2019-10-04 12:03:49 -0400

    getpics.sh

--- bash/findsetidfiles.sh
+++ bash/findsetidfiles.sh
@@ -15,5 +15,5 @@ find / -type f -executable -perm -4000 -ls 2>/dev/null | sort -k 3
 echo "-setgid files"
 find / -type f -executable -perm -2000 -ls 2>/dev/null | sort -k 3
 
-echo"----"
-find /2>dev/null -type f -exec ls -l --block-size=M {} + | sort -hr -k 5 | head -n 10 | awk '{print $5 ,$3, $9}'
+echo "==============="
+find / -type f -exec  ls -l  --block-size=M {} + 2>/dev/null | sort -rh -k 5 | head -n 10
